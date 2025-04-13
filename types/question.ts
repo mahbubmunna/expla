132b666890b20624fcc4b21ad5c47a9b20ce662e
Mahbub Hassan <mahbubhassanmunna@gmail.com>
2025-04-13 15:05:48 +0000

    Refine data model

--- types/question.ts
+++ types/question.ts
@@ -1,1 +1,12 @@
-export interface Question { id: string; }
+export type Tag = 'Work' | 'Life' | 'Tech' | 'Random' | string;
+
+export interface Question {
+    id: string;
+    audioUri: string; // Path to local file
+    createdAt: number; // Timestamp
+    tags: Tag[];
+    revisitCount: number;
+    lastRevisitedAt?: number;
+    status: 'active' | 'archived' | 'answered';
+    durationMs?: number; // Optional duration of the clip
+}
